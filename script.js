55f8d30cbcc0fc9bbd7b0d600dd232fd09cc82b5
JASIK-hub <zhasik7002@gmail.com>
2024-12-11 15:19:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+const image1=document.getElementById('image1')
+const image2=document.getElementById('image2')
+const image3=document.getElementById('image3')
+const image4=document.getElementById('image4')
+const image5=document.getElementById('image5')
+const image6=document.getElementById('image6')
+const image7=document.getElementById('image7')
+const image8=document.getElementById('image8')
+const image9=document.getElementById('image9')
+
+image1.addEventListener('mouseover', ()=>{
+    image1.src='cropped_1_1.jpg'
+    image2.src='cropped_1_2.jpg'
+    image3.src='cropped_1_2.jpg'
+    image4.src='bottom_center_face.jpg'
+    image5.src='bottom_right_face.jpg'
+    image6.src='bottom_right_face.jpg'
+    image7.src='bottom_center_face.jpg'
+    image8.src='bottom_right_face.jpg'
+})
+
+image2.addEventListener('mouseover', ()=>{
+    image1.src="cropped_1_0.jpg"
+    image2.src='cropped_1_1.jpg'
+    image3.src='cropped_1_2.jpg'
+    image4.src="cropped_2_0.jpg"
+    image5.src="bottom_center_face.jpg"
+    image6.src='bottom_right_face.jpg'
+    image7.src="cropped_2_0.jpg"
+    image8.src="bottom_center_face.jpg"
+    image9.src='bottom_right_face.jpg'
+})
+
+image3.addEventListener('mouseover', ()=>{
+    image1.src="cropped_1_0.jpg"
+    image2.src="cropped_1_0.jpg"
+    image3.src='cropped_1_1.jpg'
+    image4.src="cropped_2_0.jpg"
+    image5.src="cropped_2_0.jpg"
+    image6.src='bottom_center_face.jpg'
+    image7.src="cropped_2_0.jpg"
+    image8.src="cropped_2_0.jpg"
+    image9.src='bottom_center_face.jpg'
+})
+
+image4.addEventListener('mouseover', ()=>{
+    image1.src="cropped_0_1.jpg"
+    image2.src="cropped_0_2.jpg"
+    image3.src="cropped_0_2.jpg"
+    image4.src="cropped_1_1.jpg"
+    image5.src='cropped_1_2.jpg'
+    image6.src='cropped_1_2.jpg'
+    image7.src='bottom_center_face.jpg'
+    image8.src='bottom_right_face.jpg'
+    image9.src='bottom_right_face.jpg'
+})
+
+image5.addEventListener('mouseover', ()=>{
+    image1.src="cropped_0_0.jpg"
+    image2.src="cropped_0_1.jpg"
+    image3.src="cropped_0_2.jpg"
+    image4.src="cropped_1_0.jpg"
+    image5.src='cropped_1_1.jpg'
+    image6.src="cropped_1_2.jpg"
+    image7.src="cropped_2_0.jpg"
+    image8.src='bottom_center_face.jpg'
+    image9.src='bottom_right_face.jpg'
+})
+image6.addEventListener('mouseover', ()=>{
+    image1.src='cropped_0_0.jpg'
+    image2.src="cropped_0_0.jpg"
+    image3.src="cropped_0_1.jpg"
+    image4.src="cropped_1_0.jpg"
+    image5.src='cropped_1_0.jpg'
+    image6.src="cropped_1_1.jpg"
+    image7.src="cropped_2_0.jpg"
+    image8.src="cropped_2_0.jpg"
+    image9.src='bottom_center_face.jpg'
+})
+
+image7.addEventListener('mouseover', ()=>{
+    image1.src='cropped_0_1.jpg'
+    image2.src="cropped_0_2.jpg"
+    image3.src="cropped_0_2.jpg"
+    image4.src="cropped_0_1.jpg"
+    image5.src='cropped_0_2.jpg'
+    image6.src="cropped_0_2.jpg"
+    image7.src="cropped_1_1.jpg"
+    image8.src="cropped_1_2.jpg"
+    image9.src="cropped_1_2.jpg"
+})
+image8.addEventListener('mouseover', ()=>{
+    image1.src='cropped_0_0.jpg'
+    image2.src="cropped_0_1.jpg"
+    image3.src="cropped_0_2.jpg"
+    image4.src="cropped_0_0.jpg"
+    image5.src='cropped_0_1.jpg'
+    image6.src="cropped_0_2.jpg"
+    image7.src="cropped_1_0.jpg"
+    image8.src="cropped_1_1.jpg"
+    image9.src="cropped_1_2.jpg"
+})
+
+image9.addEventListener('mouseover', ()=>{
+    image1.src='cropped_0_0.jpg'
+    image2.src="cropped_0_0.jpg"
+    image3.src="cropped_0_1.jpg"
+    image4.src="cropped_0_0.jpg"
+    image5.src='cropped_0_0.jpg'
+    image6.src="cropped_0_1.jpg"
+    image7.src="cropped_1_0.jpg"
+    image8.src="cropped_1_0.jpg"
+    image9.src="cropped_1_1.jpg"
+})
